36584c30f10bc2b3a852144f1a840d0a8c73af5c
Ben Tanen <ben-tanen@gmail.com>
2021-02-27 10:10:39 -0700

    added code to scrape actual caption tracks (to compare asr vs. usr)

---
+++ code/scrape-captions-youtubedl.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import os
+import youtube_dl
+
+os.chdir("/Users/ben-tanen/Desktop/Projects/yt-captions")
+
+opts = {
+    "skip_download": True, 
+    "writesubtitles": True,
+    "writeautomaticsub": False,
+    "subtitleslangs": ["en", "en-US"],
+    "geo_bypass": "US",
+    "outtmpl": os.getcwd() + "/captions/%(id)s.user.%(ext)s"
+}
+
+opts_auto = dict(opts)
+opts_auto.update({"writeautomaticsub": True,
+                  "outtmpl": os.getcwd() + "/data/captions/%(id)s.auto.%(ext)s"})
+
+yt = youtube_dl.YoutubeDL(opts)
+yt_auto = youtube_dl.YoutubeDL(opts_auto)
+
+vid_ids = ["hqvOcr0uu9o", "2l14XtJwVUc", "4b33NTAuF5E"]
+vid_ids = ["ymyY7jez0rM"]
+
+youtube_dl.YoutubeDL({"listsubtitles": True, "print_json": True}).download(["https://www.youtube.com/watch?v=ymyY7jez0rM"])
+
+yt.download(["https://www.youtube.com/watch?v=%s" % vid for vid in vid_ids])
+yt_auto.download(["https://www.youtube.com/watch?v=%s" % vid for vid in vid_ids])
