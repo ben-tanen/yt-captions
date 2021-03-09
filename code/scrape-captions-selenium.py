36f1ed86e4469d9a7d702f11240a649b519a135c
ben-tanen <ben.tanen@gmail.com>
2021-03-09 09:10:28 -0700

    updated selenium scraper to work for multiple videos

--- code/scrape-captions-selenium.py
+++ code/scrape-captions-selenium.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os, sys, time, re
+import os, sys, time, re, traceback
 import pandas as pd
 
 from selenium import webdriver
@@ -12,6 +12,16 @@ from webdriver_manager.chrome import ChromeDriverManager
 #=========================#
 # DEFINE HELPER FUNCTIONS #
 #=========================#
+
+def parse_argv():
+    pattern = "--([A-z0-9]+)=(([A-z0-9]|\.|_|-|/)+)"
+    if len(sys.argv) > 1:
+        parsed_args_list = [{"key": re.match(pattern, arg)[1],
+                             "value": re.match(pattern, arg)[2]} for arg in sys.argv[1:]]
+        parsed_args = {obj["key"]: obj["value"] for obj in parsed_args_list}
+        return parsed_args
+    else:
+        return {}
 
 def exit_program(msg):
     print(msg)
@@ -83,20 +93,19 @@ def parse_timecode(timecode):
                 "total_seconds": segments[0] * 60 + segments[1]}
     
 def playing_status():
-    return play_btn.get_attribute("title")
+    return driver.find_element_by_xpath("//button[contains(@class, 'ytp-play-button')]").get_attribute("title")
 
 def pause_video():
     if "Pause" in playing_status():
-        # play_btn.click()
-        video.send_keys("k")
+        driver.find_element_by_id('movie_player').send_keys("k")
         
 def play_video():
     if "Play" in playing_status():
-        video.send_keys("k")
+        driver.find_element_by_id('movie_player').send_keys("k")
 
 def replay_video():
     if "Replay" in playing_status():
-        video.send_keys("k")
+        driver.find_element_by_id('movie_player').send_keys("k")
         
 def handle_ad(loud):
     skip_ad_text = driver.find_elements_by_xpath("//span[contains(@class, 'ytp-ad-preview-container')]")
@@ -122,7 +131,90 @@ def handle_ad(loud):
     else:
         print("no luck on ads... waiting it out")
         time.sleep(30)
-    
+
+def scrape_video_caption_text(video_id):
+    # navigate to video
+    print("navigating to video")
+    driver.get("https://www.youtube.com/watch?v=%s" % video_id)
+    time.sleep(3)
+
+    # init elements
+    video = driver.find_element_by_id('movie_player')
+    left_ctrls = driver.find_element_by_xpath("//div[contains(@class, 'ytp-left-controls')]")
+    right_ctrls = driver.find_element_by_xpath("//div[contains(@class, 'ytp-right-controls')]")
+    play_btn = left_ctrls.find_element_by_xpath("//button[contains(@class, 'ytp-play-button')]")
+
+    # attempt to click large play button
+    lg_play_btn = driver.find_elements_by_xpath("//button[contains(@class, 'ytp-large-play-button')]")
+    if len(lg_play_btn) > 0:
+        try:
+            print("clicking large play button")
+            lg_play_btn[0].click()
+        except:
+            print("tried clicking the big play button but no luck")
+
+    # attempt to handle any ads
+    handle_ad(True)
+    time.sleep(2)
+
+    # clear YT's popups
+    try:
+        left_ctrls.find_element_by_xpath("span[contains(@class, 'ytp-volume-area')]/button[contains(@class, 'ytp-mute-button')]").click()
+    except:
+        print("trying to clear popup in the way")
+        promo = driver.find_element_by_xpath("//div[contains(@class, 'ytd-mealbar-promo-renderer')]")
+        promo.find_element_by_xpath("div[contains(@class, 'button-container')]/*[1]").click()
+
+    # go to beginning of video
+    pause_video()
+    video.send_keys("0")
+
+    # mute video
+    print("muting video")
+    video.send_keys("m")
+
+    # turn off autoplay
+    print("turning off autoplay")
+    right_ctrls.find_elements_by_xpath("button")[0].click()
+
+    # get caption languages
+    try:
+        print("getting cc language options")
+        cc_options = get_cc_options()
+        cc_options_trim = [option for option in cc_options if "English" in option]
+        print("Caption languages: %s" % cc_options_trim)
+    except:
+        exit_program("No captions available... exiting...")
+
+    # scrape captions from video into cc_data array
+    cc_data = [ ]
+    for lang in cc_options_trim:
+        print("Scraping captions for %s" % lang)
+        pause_video()
+        video.send_keys("0")
+        time.sleep(1)
+        pick_cc_option(lang)
+        time.sleep(3)
+        while "Replay" not in playing_status():
+            time.sleep(0.25)
+            play_video()
+            time.sleep(0.5)
+            handle_ad(False)
+            pause_video()
+            time.sleep(0.25)
+            cc_obj = {"video_id": video_id,
+                    "lang": lang, 
+                    "text": get_cc_text(), 
+                    "timecode": get_current_timecode()["full"]}
+            print(cc_obj)
+            cc_data.append(cc_obj)
+        print("replaying video")
+        replay_video()
+
+    # save results to data folder
+    df = pd.DataFrame(cc_data).drop_duplicates()
+    print("saved %d queries as %d unique rows of cc data" % (len(cc_data), df.shape[0]))
+    df.to_csv("data/caption_text/caption_text_%s.csv" % video_id, index = False)
 
 #================#
 # SET UP BROWSER #
@@ -132,123 +224,56 @@ def handle_ad(loud):
 if os.getcwd()[-11:] != "yt-captions":
     os.chdir("/Users/ben-tanen/Desktop/Projects/yt-captions")
 
-# get video ID
-if len(sys.argv) == 1:
-    exit_program("No video IDs given... exiting...")
-elif len(sys.argv) > 2:
-    exit_program("More than one argument given... exiting...")
-elif len(sys.argv[1]) != 11:
-    exit_program("Provided video ID not proper format... exiting...")
-    
-video_id = sys.argv[1]
+# parse args (determine if file or single id)
+parsed_args = parse_argv()
+
+if 'file' in parsed_args:
+    print('using file %s for ids' % parsed_args['file'])
+    try:
+        with open(parsed_args["file"], "r") as fp:
+            lines_orig = fp.readlines()
+            video_ids = [l.replace("\n", "") for l in lines_orig]
+    except:
+        print("failed to parse input file")
+        sys.exit(1)
+elif 'id' in parsed_args:
+    print('using id %s' % parsed_args['id'])
+    if len(parsed_args["id"]) != 11:
+        print("provided video ID not proper format... exiting...")
+        sys.exit(1)
+    video_ids = [parsed_args['id']]
+else:
+    print("improper input provide (either need file or id)")
+    sys.exit(1)
+
+# check that video ids are of proper format
+for video_id in video_ids:
+    if len(video_id) != 11:
+        print("provided video ID (%s) not proper format... exiting..." % video_id)
+        sys.exit(1)
+
+# only scrape caption_text from videos we don't already have
+video_ids = [video_id for video_id in video_ids if "caption_text_%s.csv" % video_id not in os.listdir("data/caption_text/")]
+if len(video_ids) == 0:
+    print("no video ids left to scrape (none given or all already scraped)")
+    sys.exit(1)
+
+print("video id(s): %s" % video_ids)
 
 # start headless browser
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(ChromeDriverManager().install(), options = chrome_options)
 
-#===================#
-# NAVIGATE TO VIDEO #
-#===================#
-
-print("navigating to video")
-
-driver.get("https://www.youtube.com/watch?v=%s" % video_id)
-
-time.sleep(3)
-
-video = driver.find_element_by_id('movie_player')
-left_ctrls = driver.find_element_by_xpath("//div[contains(@class, 'ytp-left-controls')]")
-right_ctrls = driver.find_element_by_xpath("//div[contains(@class, 'ytp-right-controls')]")
-play_btn = left_ctrls.find_element_by_xpath("button[contains(@class, 'ytp-play-button')]")
-
-#=======================#
-# CHECK FOR PLAY BUTTON #
-#=======================#
-
-lg_play_btn = driver.find_elements_by_xpath("//button[contains(@class, 'ytp-large-play-button')]")
-if len(lg_play_btn) > 0:
+# scrape caption text for each video id
+for video_id in video_ids:
     try:
-        print("clicking large play button")
-        lg_play_btn[0].click()
+        print("==========================")
+        print("scraping caption text from %s" % video_id)
+        scrape_video_caption_text(video_id)
     except:
-        print("tried clicking the big play button but no luck")
+        print("failed to scrape caption text from %s" % video_id)
+        traceback.print_exc()
 
-#=====================================#
-# CHECK FOR AD(s) AND ATTEMPT TO SKIP #
-#=====================================#
-
-handle_ad(True)
-    
-time.sleep(2)
-
-#================#
-# CLEAR YT POPUP #
-#================#
-
-try:
-    left_ctrls.find_element_by_xpath("span[contains(@class, 'ytp-volume-area')]/button[contains(@class, 'ytp-mute-button')]").click()
-except:
-    print("trying to clear popup in the way")
-    promo = driver.find_element_by_xpath("//div[contains(@class, 'ytd-mealbar-promo-renderer')]")
-    promo.find_element_by_xpath("div[contains(@class, 'button-container')]/*[1]").click()
-
-#================================#
-# NAVIGATE TO BEGINNING OF VIDEO #
-# AND SET UP LOOP                #
-#================================#
-
-# go to beginning of video
-pause_video()
-video.send_keys("0")
-
-# turn off autoplay
-print("turning off autoplay")
-right_ctrls.find_elements_by_xpath("button")[0].click()
-
-# get caption languages
-try:
-    print("getting cc language options")
-    cc_options = get_cc_options()
-    cc_options_trim = [option for option in cc_options if "English" in option]
-    print("Caption languages: %s" % cc_options_trim)
-except:
-    exit_program("No captions available... exiting...")
-
-# scrape captions from video into cc_data array
-cc_data = [ ]
-for lang in cc_options_trim:
-    print("Scraping captions for %s" % lang)
-    pause_video()
-    video.send_keys("0")
-    time.sleep(1)
-    pick_cc_option(lang)
-    time.sleep(3)
-    while "Replay" not in playing_status():
-        time.sleep(0.25)
-        play_video()
-        time.sleep(0.5)
-        handle_ad(False)
-        pause_video()
-        time.sleep(0.25)
-        cc_obj = {"video_id": video_id,
-                  "lang": lang, 
-                  "text": get_cc_text(), 
-                  "timecode": get_current_timecode()["full"]}
-        print(cc_obj)
-        cc_data.append(cc_obj)
-    print("replaying video")
-    replay_video()
-  
-df = pd.DataFrame(cc_data).drop_duplicates()
-
-print("saved %d queries as %d unique rows of cc data" % (len(cc_data), df.shape[0]))
-
-df.to_csv("data/caption_text/caption_text_%s.csv" % video_id, index = False)
-
+# close driver
 driver.quit()
-    
-
-
-
-
